91f036d2fdec2d14c594f916c79e7f7b0fa6bd78
Jesid Polo <jesing482@gmail.com>
2023-07-29 17:07:14 -0500

    actualizando ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,11 @@ module.exports = {
     deploy : {
       production : {
         user : 'root',
-        host : 'supertransporte_backend_prod',
+        host : 'supertransporte_backend_vista_prod',
         ref  : 'origin/main',
-        repo : 'https://github.com/jesidpolo04/backend-supertransporte',
-        path : '/var/pesvsisi/backend_core',
-        'post-deploy': 'npm install && npm run build && cp .env build/.env && cd build && npm ci --production && pm2 restart backend_core',
+        repo : 'https://github.com/juliozaor/backend-vista-vigia',
+        path : '/var/pesvsisi/backend_vista',
+        'post-deploy': 'npm install && npm run build && cp .env build/.env && cd build && npm ci --production && pm2 restart backend_vista',
       }
     }
   };
